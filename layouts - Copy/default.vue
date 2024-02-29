d958cb11becf7b1118ca34701c8fb9a8ed260996
yadav76 <93885474+yadav76@users.noreply.github.com>
2024-02-29 17:20:41 +0530

    adding project

---
+++ layouts - Copy/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Cart App</NuxtLink>
+        <ul class="flex gap-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <!-- <li><NuxtLink to="/about">About</NuxtLink></li> -->
+          <li><NuxtLink to="/products" class="btn">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- output the page content -->
+    <div class="container mx-auto p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* target active class of NuxtLink  */
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
